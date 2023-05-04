7871697440bae6329a60f6208c0010f5bba7b750
Quentin <quentin.bidolet@gmail.com>
2023-05-04 14:31:29 +0200

    - freezing mon module, init et main

---
+++ cours/07-freezing-packaging/freezing/mon_app/main.py
@@ -0,0 +1,3 @@
+from mon_app import hello_world
+
+hello_world()
---
+++ cours/07-freezing-packaging/freezing/mon_app/mon_app/__init__.py
@@ -0,0 +1,9 @@
+"""
+    Le fichier __init__.py est obligatoire pour que Python reconnaisse un dossier en tant que module.
+    C'est un fichier spécial, il faut respecter le nom exacte.
+    Il est exécuté quand le module est importé.
+    Il peut être vide ou contenir du code d'initialisation et des importations.
+"""
+
+# exemple d'importation
+from .mon_module import hello_world
