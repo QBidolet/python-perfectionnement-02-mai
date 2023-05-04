f59f4c1d0a85eae875c1755d386b9e15b781b354
Quentin <quentin.bidolet@gmail.com>
2023-05-04 11:46:59 +0200

    - tracer

---
+++ cours/06-monitoring/03-trace.py
@@ -0,0 +1,18 @@
+# trace permet de suivre l'exécution du code ligne par ligne.
+# Il affiche les fonctions exécutées et les fonctions appelées.
+
+import trace
+
+
+def recursive_function(n):
+    if n <= 0:
+        return 0
+    else:
+        return n + recursive_function(n - 1)
+
+
+tracer = trace.Trace(count=True, trace=True)
+tracer.runfunc(recursive_function, 5)
+
+results = tracer.results()
+results.write_results(coverdir="trace_report", show_missing=True, summary=True)
