7659f80d6fc1e6f5cef7474e6cbd0f0203e899b4
Quentin <quentin.bidolet@gmail.com>
2023-05-05 15:31:32 +0200

    - database

---
+++ cours/TP FINAL/database.py
@@ -0,0 +1,25 @@
+import sqlite3
+from contextlib import contextmanager
+
+
+@contextmanager
+def database_connection():
+    connection = sqlite3.connect("app.db")
+    cursor = connection.cursor()
+
+    try:
+        yield cursor
+    finally:
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+
+def create_table():
+    with database_connection() as cursor:
+        cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, user_id INTEGER, text TEXT)")
+
+
+if __name__ == '__main__':
+    create_table()
